1dd604f28eec7babc39769fdbd47f6d66678d796
emeraldingg <59565612+emeraldingg@users.noreply.github.com>
2023-05-21 11:21:09 +0200

    Switched to the cataas.com random cat API

--- cmds/katze.js
+++ cmds/katze.js
@@ -7,21 +7,34 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("katze")
         .setDescription("Zeigt ein zufälliges Katzenbild an."),
-    async execute(msg) {
-        try {
-            const catObj = await (await fetch("https://aws.random.cat/meow")).json();
-            const embed = new Discord.EmbedBuilder()
-                .setColor(utils.randomColor())
-                .setTitle("Katze")
-                .setImage(catObj.file)
-                .setURL(catObj.file)
-                .setFooter({
-                    text: "Funktioniert mit random.cat"
-                });
-            msg.reply({ embeds: [embed] });
-        } catch (error) {
-            msg.reply("Fehler: Die Katzen schlafen gerade.");
-            console.log(error);
+    async execute(msg, args) {
+        let catUrl;
+        if(!args[0]) {
+            try {
+                const catObj = await (await fetch("https://cataas.com/cat?json=true")).json();
+                catUrl = `https://cataas.com/${catObj.url}`;
+            } catch (error) {
+                msg.reply("Fehler: Die Katzen schlafen gerade.");
+                console.log(error);
+            }
+        } else {
+            try {
+                let text = args.join(" ").replace(/[\u00A0-\u9999<>&]/g, i => "&#"+i.charCodeAt(0)+";");
+                const catObj = await (await fetch(`https://cataas.com/cat/says/${text}?json=true`)).json();
+                catUrl = `https://cataas.com/${catObj.url}`;
+            } catch (error) {
+                msg.reply("Fehler: Die Katzen schlafen gerade.");
+                console.log(error);
+            }
         }
+        const embed = new Discord.EmbedBuilder()
+            .setColor(utils.randomColor())
+            .setTitle("Katze")
+            .setImage(catUrl)
+            .setURL(catUrl)
+            .setFooter({
+                text: "Funktioniert mit cataas.com"
+            });
+        msg.reply({ embeds: [embed] });
     },
 };
