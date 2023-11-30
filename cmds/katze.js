c097b1d0bfc2cfe1bf11ca63a2f21a9d4678b718
emeraldingg <59565612+emeraldingg@users.noreply.github.com>
2023-11-30 18:49:24 +0100

    Adapted the cat command to API changes

--- cmds/katze.js
+++ cmds/katze.js
@@ -8,33 +8,27 @@ module.exports = {
         .setName("katze")
         .setDescription("Zeigt ein zufälliges Katzenbild an."),
     async execute(msg, args) {
-        let catUrl;
-        if(!args[0]) {
-            try {
-                const catObj = await (await fetch("https://cataas.com/cat?json=true")).json();
-                catUrl = `https://cataas.com/${catObj.url}`;
-            } catch (error) {
-                msg.reply("Fehler: Die Katzen schlafen gerade.");
-                console.log(error);
+        try {
+            const catObj = await (await fetch("https://cataas.com/cat?json=true")).json();
+            let catUrl;
+            if(!args[0]) {
+                catUrl = `https://cataas.com/cat/${catObj._id}`;
+            } else {
+                let text = encodeURIComponent(args.join(" "));
+                catUrl = `https://cataas.com/cat/${catObj._id}/says/${text}`;
             }
-        } else {
-            try {
-                let text = args.join(" ").replace(/[\u00A0-\u9999<>&]/g, i => "&#"+i.charCodeAt(0)+";");
-                const catObj = await (await fetch(`https://cataas.com/cat/says/${text}?json=true`)).json();
-                catUrl = `https://cataas.com/${catObj.url}`;
-            } catch (error) {
-                msg.reply("Fehler: Die Katzen schlafen gerade.");
-                console.log(error);
-            }
+            const embed = new Discord.EmbedBuilder()
+                .setColor(utils.randomColor())
+                .setTitle("Katze")
+                .setImage(catUrl)
+                .setURL(catUrl)
+                .setFooter({
+                    text: "Funktioniert mit cataas.com"
+                });
+            msg.reply({ embeds: [embed] });
+        } catch (error) {
+            msg.reply("Fehler: Die Katzen schlafen gerade.");
+            console.log(error);
         }
-        const embed = new Discord.EmbedBuilder()
-            .setColor(utils.randomColor())
-            .setTitle("Katze")
-            .setImage(catUrl)
-            .setURL(catUrl)
-            .setFooter({
-                text: "Funktioniert mit cataas.com"
-            });
-        msg.reply({ embeds: [embed] });
     },
 };
